8d763ce84d2cef7ad40c06594603f739d2d53fa8
Sunshine2504 <67236835+Sunshine2504@users.noreply.github.com>
2021-12-12 01:19:23 +0300

    Add panels components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <list-panel></list-panel>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListPanel from "./components/ListPanel.vue"
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    ListPanel
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ListPanel.vue
@@ -0,0 +1,103 @@
+<template>
+  <ul class="lists">
+    <li v-for="(currentList, index) in lists" :key="index">
+      <h2>List {{index + 1}}</h2>
+      <ul class="hideContent accordion" :class="{'accordion-active': isHide == false}">
+        <li v-for="item in currentList" :key="item.name">
+          <p>{{ item.name }}</p>
+          <input type="text" v-model="item.count" />
+          <input type="color" v-model="item.color" />
+        </li>
+      </ul>
+    </li>
+
+    <!-- <li>
+      <h2>List 2</h2>
+      <ul class="hideContent accordion" :class="{'accordion-active': isHide == false}">
+        <li v-for="item in list2" :key="item.name">
+          <p>{{ item.name }}</p>
+          <input type="text" v-model="item.count" />
+          <input type="color" v-model="item.color" />
+        </li>
+      </ul>
+    </li>
+
+    <li>
+      <h2>List 3</h2>
+      <ul class="hideContent accordion" :class="{'accordion-active': isHide == false}">
+        <li v-for="item in list3" :key="item.name">
+          <p>{{ item.name }}</p>
+          <input type="text" v-model="item.count" />
+          <input type="color" v-model="item.color" />
+        </li>
+      </ul>
+    </li> -->
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isHide: false,
+
+      list1: [
+        { name: "Item 1", color: "", count: null },
+
+        { name: "Item 2", color: "", count: null },
+
+        { name: "Item 3", color: "", count: null },
+
+        { name: "Item 4", color: "", count: null },
+      ],
+
+      list2: [
+        { name: "Item 1", color: "", count: null },
+
+        { name: "Item 2", color: "", count: null },
+
+        { name: "Item 3", color: "", count: null },
+
+        { name: "Item 4", color: "", count: null },
+
+        { name: "Item 5", color: "", count: null },
+      ],
+      list3: [
+        { name: "Item 1", color: "", count: null },
+
+        { name: "Item 2", color: "", count: null },
+
+        { name: "Item 3", color: "", count: null },
+
+        { name: "Item 4", color: "", count: null },
+
+        { name: "Item 5", color: "", count: null },
+
+        { name: "item 6", color: "", count: null },
+      ],
+
+      lists: [this.list1, this.list2, this.list3]
+    };
+  },
+  methods: {
+    changeAccordion () {
+      this.isHide = !this.isHide
+    }
+  },
+};
+</script>
+<style>
+ul {
+  list-style: none;
+}
+
+.accordion {
+  overflow: hidden;
+  height: 0px;
+  transition: all 1s;
+}
+
+.accordion-active {
+  transition: all 1s;
+  height: 100%;
+}
+</style>
